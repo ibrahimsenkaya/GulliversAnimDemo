6aa13af4edb0ccff181f29a63f68c1c8ea40a079
ibrahimsenkaya <isenkaya03@gmail.com>
2020-02-17 23:11:45 +0300

    Added anims

---
+++ New Unity Project/Assets/Scripts/CardAnimController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardAnimController : MonoBehaviour
+{
+    ParticleSystem part;
+    [SerializeField] Sprite sp;
+    void Awake()
+    {
+        part = GetComponent<ParticleSystem>();
+    }
+
+
+    public void CardPartAnim()
+    {
+        part.Play();
+        for (int i = 0; i < 2; i++)
+        {
+            transform.GetChild(i).transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = sp;
+        }
+      
+    }
+
+}
---
+++ New Unity Project/Assets/Scripts/ChestAnimController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestAnimController : MonoBehaviour
+{
+    ParticleSystem part;
+    [SerializeField] GameObject Card;
+    void Awake()
+    {
+        part =transform.GetChild(3).GetComponent<ParticleSystem>();
+     
+    }
+
+    public void PlayPartAnim()
+    {
+        part.Play();
+        Instantiate(Card, Vector3.zero, Quaternion.identity);
+    }
+}
