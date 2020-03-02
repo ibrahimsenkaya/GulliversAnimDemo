f08b676dca33912f5a22645e2d0ebeec97338916
ibrahimsenkaya <isenkaya03@gmail.com>
2020-03-02 13:54:58 +0300

    Get feedBacks

---
+++ New Unity Project/Assets/Scripts/Chest2/CardAnim.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardAnim : MonoBehaviour
+{
+    [SerializeField] ParticleSystem Chargepart;
+    [SerializeField] ParticleSystem Boom1,Boom2,Boom3,Flies;
+    [SerializeField] GameObject Skin1, Skin2;
+    [SerializeField] Sprite sp;
+    [SerializeField] GameObject Glow;
+
+
+ 
+
+    public void ChargeIt()
+    {
+        Instantiate(Glow, new Vector3(0,.2f,-1f), Quaternion.identity);
+        Chargepart.Play();
+      
+    }
+    public void Boom()
+    {
+        Boom1.Play();
+        Boom2.Play();
+        Boom3.Play();
+        Flies.Play();
+
+    }
+    public void ShowSkin()
+    {
+        Skin1.GetComponent<SpriteRenderer>().sprite = sp;
+
+        Skin2.GetComponent<SpriteRenderer>().sprite = sp;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Chest2/ChestAnim.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestAnim : MonoBehaviour
+
+{
+    [SerializeField] GameObject Card;
+    [SerializeField] ParticleSystem part1, part2,part3,part4;
+    public void yaz()
+    {
+      
+        Instantiate(Card, Vector3.zero, Quaternion.identity);
+        part1.Play();
+        part2.Play();
+        part3.Play();
+        part4.Play();
+    }
+}
